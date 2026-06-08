66d20c92f82054122bf3e0bbcad07e47f4efdbb3
Sif El Din Deabes <62103437+Mas-Deabes@users.noreply.github.com>
2026-06-08 18:40:54 +0100

    Day 1 project completed

--- day1.py
+++ day1.py
@@ -1,1 +1,7 @@
-print("hello world")
+print("Welcome to the Band Name Generator.")
+
+cityName = input(str("What's the name of the city you grew up in ? "))
+
+petName = input(str("What's your pet's name? "))
+
+print ("your band name is "+ cityName + " " + petName)
