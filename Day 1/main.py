0fbd8bd1066dd3d7f04dfbaff675834e0d6aed3e
Sif El Din Deabes <62103437+Mas-Deabes@users.noreply.github.com>
2026-06-08 20:40:59 +0100

    rearranging course files

--- Day 1/main.py
+++ Day 1/main.py
@@ -3,4 +3,4 @@
 print("Welcome to the Band Name Generator.")
 cityName = input(str("What's the name of the city you grew up in ? "))
 petName = input(str("What's your pet's name? "))
-print ("your band name is "+ cityName + " " + petName)
+print (f"your band name is {cityName} {petName}")
